a4d17e23a8176f8a91d2889d62c221d017c0b6f4
vincent doquang <vincent.doquang-prestataire@laposte.fr>
2019-07-12 18:35:24 +0200

    preparation for other behaviour

--- v2/libIa.js
+++ v2/libIa.js
@@ -8,6 +8,8 @@ function DefaultBehaviour(object) {
  }
 }
 
+// SIMPLE MOVEMENT BEHAVIOUR ///////////////////////////////////////////////////
+
 function ChasingBehaviour(object) {
   this.object = object;
 
@@ -27,8 +29,6 @@ function ChasingBehaviour(object) {
     } else {
       var thetaNow = Math.min(thetaDiff, TURNOVER_SPEED_FACTOR_MS * this.object.timeKeeper.update);
     }
-    //console.log("dirrection angle: " + thetaCurrent.toFixed(2));
-    //console.log("correction angle: " + thetaNow.toFixed(2));
     // Apply corrective angle
     var diffX = -1*Math.cos(thetaCurrent + thetaNow);
     var diffY = -1*Math.sin(thetaCurrent + thetaNow);
@@ -98,13 +98,33 @@ function FleeingBehaviour(object) {
     // Save new speed
     this.object.last.dh = this.object.velocity.h;
     this.object.last.dv = this.object.velocity.v;
-    //this.object.velocity.h = diffH;
-    //this.object.velocity.v = diffV;
+    this.object.velocity.h = diffH;
+    this.object.velocity.v = diffV;
   }
 }
 
+function OntoesBehaviour(object) {
+  this.object = object;
 
-function FireAtWill(object) {
+  this.move = function move() {
+    // TODO
+    // If object in vital space, try to dodge the bullet, else, return to safe point
+  }
+}
+
+function WanderingBehaviour(object) {
+  this.object = object;
+
+  this.move = function move() {
+    // TODO
+    // Define a seed, then a function of time to describe the path
+    // (make an radius projection of a null gain function?)
+  }
+}
+
+// SIMPLE FIRING BEHAVIOUR /////////////////////////////////////////////////////
+
+function FireAtWillBehaviour(object) {
   this.object = object;
 
   this.move = function move() {
@@ -121,31 +141,96 @@ function FireAtWill(object) {
   }
 }
 
-
-function ComplexeBehaviourHunter(object) {
+function UnleashBehaviour(object) {
   this.object = object;
-  this.chasingBehaviour = new ChasingBehaviour(object);
-  this.fireAtWill = new FireAtWill(object);
-  this.fleeingBehaviour = new FleeingBehaviour(object);
 
   this.move = function move() {
-    // Determine behaviour to have dependending on the distance to target
-    var distance = Math.sqrt(Math.pow(this.object.hitbox.h - this.object.target.hitbox.h,2) + Math.pow(this.object.hitbox.v - this.object.target.hitbox.v,2));
-    if(distance < PASSERBY_SPAWN_CIRCLE) {
-      this.fleeingBehaviour.move();
-    } else {
-      this.chasingBehaviour.move();
+    // Always fire when ready
+    if(this.object.timeKeeper.seen - this.object.last.fire > 1000/this.object.weapon.fireRate * 3  /*firerate factor for IA*/
+      && Date.now()%(this.object.weapon.fireRate * 10) <6) {
+      // Define projectile state
+      var initVelN = Math.sqrt(Math.pow(this.object.target.hitbox.h - this.object.hitbox.h,2) + Math.pow(this.object.target.hitbox.v - this.object.hitbox.v,2));
+      var initVel = initVelN == 0 ? {h:0, v:0} : {h: (this.object.target.hitbox.h - this.object.hitbox.h) / initVelN * this.object.weapon.velocity, v: (this.object.target.hitbox.v - this.object.hitbox.v) / initVelN * this.object.weapon.velocity}
+      // Confirm creation of projectile
+      projectileFactory.spawn(this.object.hitbox, initVel, this.weapon);
+      // Update firing state
+      this.object.last.fire = Date.now();
     }
-    this.fireAtWill.move();
+  }
+}
+
+function HoldBehaviour(object) {
+  this.object = object;
+
+  this.move = function move() {
+    // Hold fire
+  }
+}
+
+function SnipeBehaviour(object) {
+  this.object = object;
+
+  this.move = function move() {
+    // Fire when reaching maximum heat
+    if(this.object.timeKeeper.seen - this.object.last.fire > 1000/this.object.weapon.fireRate * 6 /*firerate factor for IA*/) {
+      // Define projectile state
+      var initVelN = Math.sqrt(Math.pow(this.object.target.hitbox.h - this.object.hitbox.h,2) + Math.pow(this.object.target.hitbox.v - this.object.hitbox.v,2));
+      var projection = {h: 0, v: 0}; // TODO aim at the point of impact
+      var initVel = initVelN == 0 ? {h:0, v:0} : {h: (this.object.target.hitbox.h - this.object.hitbox.h) / initVelN * this.object.weapon.velocity, v: (this.object.target.hitbox.v - this.object.hitbox.v) / initVelN * this.object.weapon.velocity}
+      // Confirm creation of projectile
+      projectileFactory.spawn(this.object.hitbox, initVel, this.weapon);
+      // Update firing state
+      this.object.last.fire = Date.now();
+    }
+  }
+}
+
+function SuppressBehaviour(object) {
+  this.object = object;
+
+  this.move = function move() {
+    // Randomly fire (following a seed) if able (seed based on fireate)
+    if(this.object.timeKeeper.seen - this.object.last.fire > 1000/this.object.weapon.fireRate * 5 /*firerate factor for IA*/
+      && Math.floor(Math.random()*1000) > 990) {
+      // Define projectile state
+      var initVelN = Math.sqrt(Math.pow(this.object.target.hitbox.h - this.object.hitbox.h,2) + Math.pow(this.object.target.hitbox.v - this.object.hitbox.v,2));
+      // Aim at a random point around the target
+      var deviation = {h: (this.object.target.hitbox.h - this.object.hitbox.h) * (1+ (Math.floor(Math.random()*20) -10)/100),
+        v: (this.object.target.hitbox.v - this.object.hitbox.v) * (1+ (Math.floor(Math.random()*20) -10)/100)};
+      var initVel = initVelN == 0 ? {h:0, v:0} : {h: deviation.h / initVelN * this.object.weapon.velocity, v: deviation.v / initVelN * this.object.weapon.velocity}
+      // Confirm creation of projectile
+      projectileFactory.spawn(this.object.hitbox, initVel, this.weapon);
+      // Update firing state
+      this.object.last.fire = Date.now();
+    }
+  }
+}
+
+// COMPLEX BEHAVIOUR ///////////////////////////////////////////////////////////
+
+function ComplexeBehaviourSkirmish(object) {
+  this.object = object;
+  this.dodgingBehaviour = new DodgingBehaviour(object);
+  this.chasingBehaviour = new ChasingBehaviour(object);
+  this.fleeingBehaviour = new FleeingBehaviour(object);
+  this.snipeBehaviour = new SnipeBehaviour(object);
+  this.suppressBehaviour = new SuppressBehaviour(object);
+  this.unleashBehaviour = new UnleashBehaviour(object);
+
+  this.move = function move() {
+    //alternate between flee and snipe if close, dodge and suppress if long, and chase and unleash
+
   }
 }
 
 function ComplexeBehaviourHarrier(object) {
   this.object = object;
   this.chasingBehaviour = new ChasingBehaviour(object);
-  this.fireAtWill = new FireAtWill(object);
   this.fleeingBehaviour = new FleeingBehaviour(object);
   this.orbitingBehaviour = new OrbitingBehaviour(object);
+  this.fireAtWillBehaviour = new FireAtWillBehaviour(object);
+  this.snipeBehaviour = new SnipeBehaviour(object);
+  this.suppressBehaviour = new SuppressBehaviour(object);
   this.inConfortZone = false;
 
   this.move = function move() {
@@ -153,14 +238,61 @@ function ComplexeBehaviourHarrier(object) {
     var distance = Math.sqrt(Math.pow(this.object.hitbox.h - this.object.target.hitbox.h,2) + Math.pow(this.object.hitbox.v - this.object.target.hitbox.v,2));
     if(distance < PASSERBY_SPAWN_CIRCLE * .65) {
       this.fleeingBehaviour.move();
+      this.suppressBehaviour.move();
       this.inConfortZone = false;
     } else if(distance > PASSERBY_SPAWN_CIRCLE * 1.35) {
       this.chasingBehaviour.move();
+      this.snipeBehaviour.move();
       this.inConfortZone = false;
     } else {
       this.orbitingBehaviour.move();
+      this.fireAtWillBehaviour.move();
       this.inConfortZone = true;
     }
-    this.fireAtWill.move();
+  }
+}
+
+function ComplexeBehaviourHunter(object) {
+  this.object = object;
+  this.chasingBehaviour = new ChasingBehaviour(object);
+  this.fleeingBehaviour = new FleeingBehaviour(object);
+  this.fireAtWillBehaviour = new FireAtWillBehaviour(object);
+  this.unleashBehaviour = new UnleashBehaviour(object);
+
+  this.move = function move() {
+    // Determine behaviour to have dependending on the distance to target
+    var distance = Math.sqrt(Math.pow(this.object.hitbox.h - this.object.target.hitbox.h,2) + Math.pow(this.object.hitbox.v - this.object.target.hitbox.v,2));
+    if(distance < PASSERBY_SPAWN_CIRCLE) {
+      this.fleeingBehaviour.move();
+      this.unleashBehaviour.move();
+    } else {
+      this.chasingBehaviour.move();
+      this.fireAtWillBehaviour.move();
+    }
+  }
+}
+
+function ComplexeBehaviourHaunt(object) {
+  this.object = object;
+  this.chasingBehaviour = new ChasingBehaviour(object);
+  this.dodgingBehaviour = new DodgingBehaviour(object);
+  this.holdBehaviour = new HoldBehaviour(object);
+  this.unleashBehaviour = new UnleashBehaviour(object);
+
+  this.move = function move() {
+    // alternate randomly between chase and hold, and dodge and unleash
+
+  }
+}
+
+function ComplexeBehaviourFairy(object) {
+  this.object = object;
+  this.orbitingBehaviour = new OrbitingBehaviour(object);
+  this.dodgingBehaviour = new DodgingBehaviour(object);
+  this.snipeBehaviour = new SnipeBehaviour(object);
+  this.suppressBehaviour = new SuppressBehaviour(object);
+
+  this.move = function move() {
+    // alternate randomly between orbit and suppress, and dodge and snipe
   }
 }
